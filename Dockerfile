50efe00074bb810add7fd5a90b9a6a67585d68b5
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-10-04 15:15:28 +0900

    create page and deplopy env

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,55 @@
 # 開発環境 ver
+# FROM node:12-alpine
+
+# ENV CI=true
+
+# WORKDIR /app
+
+# COPY package.json yarn.lock ./
+
+# RUN yarn install --production --no-progress
+
+# COPY . .
+
+# CMD ["yarn", "start"]
+
+# 開発環境 ver
 FROM node:12-alpine
 
+MAINTAINER dilmnqvovpnmlib <simplelpmis6@gmail.com>
+
+ENV TZ Asia/Tokyo
 ENV CI=true
 
+# update
+RUN apk update
+RUN apk add nginx && \
+  apk add vim && \
+  apk add less && \
+  apk add supervisor && \
+  apk add openrc
+
+RUN mkdir /app
 WORKDIR /app
-
 COPY package.json yarn.lock ./
-
 RUN yarn install --production --no-progress
-
 COPY . .
+RUN yarn build
 
-CMD ["yarn", "start"]
+RUN sed -i 's/#rc_sys=""/rc_sys="lxc"/g' /etc/rc.conf
+RUN echo 'rc_provide="loopback net"' >> /etc/rc.conf
+RUN sed -i 's/^#\(rc_logger="YES"\)$/\1/' /etc/rc.conf
+RUN sed -i '/tty/d' /etc/inittab
+RUN sed -i 's/hostname $opts/# hostname $opts/g' /etc/init.d/hostname
+RUN sed -i 's/mount -t tmpfs/# mount -t tmpfs/g' /lib/rc/sh/init.sh
+RUN sed -i 's/cgroup_add_service /# cgroup_add_service /g' /lib/rc/sh/openrc-run.sh
+RUN rc-status
+RUN touch /run/openrc/softlevel
+
+# copy configuration files.
+COPY ./conf/supervisord.conf /etc/supervisord.conf
+COPY ./conf/default.conf /etc/nginx/conf.d/default.conf
+
+EXPOSE 80
+
+CMD ["/bin/sh", "./docker-entrypoint.sh"]
---
+++ src/containers/Main.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+
+import { MainComponent } from "../components/Main";
+
+interface worksDataType {
+  tagName: JSX.Element;
+  title: string;
+  body: string;
+}
+
+interface worksType {
+  key: string;
+  value: string;
+  data: Array<worksDataType>;
+}
+
+interface skillsContensType {
+  key: string;
+  value: string;
+  data: Array<string>;
+}
+
+interface researchContensType {
+  key: string;
+  value: string;
+  data: Array<string>;
+}
+
+class Main extends React.Component {
+  makeATag = (link: string, body: string): JSX.Element => (
+    <a href={link} target="_blank" rel="noreferrer noopener">
+      {body}
+    </a>
+  );
+  render() {
+    const works: worksType = {
+      key: "works",
+      value: "Works",
+      data: [
+        {
+          tagName: this.makeATag(
+            "https://www.miraikikin.osaka-u.ac.jp/donation/application",
+            "大阪大学未来基金"
+          ),
+          title: "大阪大学に寄付をするためのシステム",
+          body:
+            "HTML, CSS, TypeScript, Angular, Python, Django Rest Frameworkで作成",
+        },
+        {
+          tagName: this.makeATag(
+            "https://dilmnqvovpnmlib.github.io/dilmnqvovpnmlib/",
+            "Portfolio"
+          ),
+          title: "私のポートフォリオ(このページ)",
+          body: "React, Ant Design, GitHub Pagesで作成",
+        },
+        {
+          tagName: this.makeATag("https://store.artill.jp/", "Artill ECサイト"),
+          title: "アート作品を売買するためのECサイト",
+          body: "React, Reduxで作成中",
+        },
+        {
+          tagName: this.makeATag(
+            "https://baseball-data-park.com/",
+            "Baseball Data Park"
+          ),
+          title: "野球に関する情報情報を集めたサイト",
+          body: "React, Redux, Firebase, Python + Scrapingで作成中",
+        },
+      ],
+    };
+    const skillsContens: skillsContensType = {
+      key: "skills",
+      value: "Skills",
+      data: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "TypeScript",
+        "Python",
+        "React",
+        "Vue",
+        "Angular",
+        "Django Rest Framework",
+        "Git",
+        "Docker",
+      ],
+    };
+    const researchContens: researchContensType = {
+      key: "research",
+      value: "Research Keywords",
+      data: [
+        "光ファイバー通信",
+        "ディジタル信号処理",
+        "半導体レーザ",
+        "FPGA",
+        "ネットワーク",
+      ],
+    };
+    return (
+      <MainComponent
+        works={works}
+        skillsContens={skillsContens}
+        researchContens={researchContens}
+      />
+    );
+  }
+}
+
+export default Main;
