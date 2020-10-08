b40c5294a563d6e49e5097863b035d178a916eff
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-10-08 15:40:13 +0900

    #18 fix backend api path

--- backend/backend/api/urls.py
+++ backend/backend/api/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import ExperiencesView, WorksView, SkillsVeiw, ArticlesView, ResearchView
 
 urlpatterns = [
-    path('experiences/', ExperiencesView.as_view()),
-    path('works/', WorksView.as_view()),
-    path('skills/', SkillsVeiw.as_view()),
-    path('articles/', ArticlesView.as_view()),
-    path('research/', ResearchView.as_view()),
+    path('experiences', ExperiencesView.as_view()),
+    path('works', WorksView.as_view()),
+    path('skills', SkillsVeiw.as_view()),
+    path('articles', ArticlesView.as_view()),
+    path('research', ResearchView.as_view()),
 ]
